773e7b83932dbf333f1cb2626046983cf0115f97
Husna Rafi <husnarafi@husnas-macbook-pro.local>
2016-10-31 08:23:17 +0800

    Initial Commit

---
+++ RAFI_HUSNA_MIPS/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  RAFI_HUSNA_MIPS
+//
+//  Created by Husna Rafi on 10/31/16.
+//  Copyright © 2016 Husna Rafi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
